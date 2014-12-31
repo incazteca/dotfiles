6fb39c749020bb294daa6036b7c20327a63bfc94
Franco Reyes <freyes1988@gmail.com>
2014-12-31 00:05:03 -0600

    Setup updated to install pathogen.
    Also begin method for installing other plugins

--- setup.sh
+++ setup.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+function install_pathogen {
+    curl -LSso ~/dotfiles/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+}
+
+function setup_vim_plugins {
+    vim_plugins=(
+        'scrooloose/syntastic'
+        'scrooloose/nerdtree'
+        'tpope/vim-endwise'
+        'tpope/vim-fugitive'
+        'tpope/vim-haml'
+    )
+
+    echo $vim_plugins
+    for plugin in $vim_plugins; do
+        echo $plugin
+    done
+}
+
+# Begin Main
+
+#install_pathogen
+setup_vim_plugins
+
 # Retrieve list of dotfiles
 
 current_date=`date +%Y%m%d`
@@ -9,8 +33,10 @@ dotfiles=($(find . -maxdepth 1 -type f ! -name '*.sh' -a ! -name '*.md' -a ! -na
 # TODO: Pop out first entry in dot directories since it is the current directory
 dot_directories=($(find . -maxdepth 1 -type d ! -name 'backups' -a ! -name '.git' -a ! -name '.' -printf '%f\n'))
 
-echo ${dotfiles[@]}
-echo ${dot_directories[@]}
+#echo ${dotfiles[@]}
+#echo ${dot_directories[@]}
+
+
 # Backup dotfiles in backup directory
 
 
